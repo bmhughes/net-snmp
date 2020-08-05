5dc1b54f4755d1d4229994eaa41f7b160b3a8f9e
Benjamin M. Hughes <bmhughes@bmhughes.co.uk>
2020-08-05 18:26:24 +0100

    Version bump to 0.1.1
    Issue #5

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,10 @@
 
 This file is used to list changes made in each version of the net_snmp cookbook.
 
+### v0.1.1 (2020-08-05)
+
+- Fix missing newlines for `extend` and `proxy` directives
+
 ### v0.1.0 (2020-07-08)
 
 - Initial release
--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer 'Ben Hughes'
 maintainer_email 'bmhughes@bmhughes.co.uk'
 license 'Apache-2.0'
 description 'Installs/Configures net_snmp'
-version '0.1.0'
+version '0.1.1'
 chef_version '>= 16.0'
 
 issues_url 'https://gitlab.bmhughes.co.uk/bmhughes-net-chef/wrappers/net_snmp/issues'
