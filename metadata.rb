f6a7f7fd8f0ff2be63defdd0b32db2e21603f4cb
Benjamin M. Hughes <bmhughes@bmhughes.co.uk>
2020-11-08 22:19:42 +0000

    Update metadata URLs

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description 'Installs/Configures net_snmp'
 version '0.1.1'
 chef_version '>= 16.0'
 
-issues_url 'https://gitlab.bmhughes.co.uk/bmhughes-net-chef/wrappers/net_snmp/issues'
-source_url 'https://gitlab.bmhughes.co.uk/bmhughes-net-chef/wrappers/net_snmp'
+issues_url 'https://gitlab.bmhughes.co.uk/bmhughes-net-chef/cookbooks/net_snmp/issues'
+source_url 'https://gitlab.bmhughes.co.uk/bmhughes-net-chef/cookbooks/net_snmp'
 
 %w(centos fedora redhat debian ubuntu).each { |os| supports os }
