c98a155412ebe355e1c674b632e948a1299b7766
Benjamin M. Hughes <bmhughes@bmhughes.co.uk>
2021-05-08 19:10:06 +0100

    Version 1.0.0

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer 'Ben Hughes'
 maintainer_email 'bmhughes@bmhughes.co.uk'
 license 'Apache-2.0'
 description 'Installs/Configures net_snmp'
-version '0.1.1'
+version '1.0.0'
 chef_version '>= 16.0'
 
 issues_url 'https://gitlab.bmhughes.co.uk/bmhughes-net-chef/cookbooks/net_snmp/issues'
